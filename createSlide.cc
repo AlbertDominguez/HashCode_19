031ca86c30aa5eab1bc7f33af02bcb1532217163
paumarquez <pmarquezdrums@gmail.com>
2019-02-28 20:16:52 +0100

    Create Slide function

---
+++ createSlide.cc
@@ -0,0 +1,40 @@
+#include "structures.hh"
+#include <algorithm>
+#include <iostream>
+
+Slide createSlide(Picture p1, Picture p2) {
+    Slide ans;
+    ans.ph1 = p1.id;
+    ans.ph2 = p1.id;
+    set_union(p1.tags.begin(), p1.tags.end(), 
+               p2.tags.begin(), p2.tags.end(), 
+               inserter(ans.tags, ans.tags.begin()));
+    return ans;
+}
+
+Slide createSlide(Picture p1) {
+    Slide ans;
+    ans.ph1 = p1.id;
+    ans.ph2 = -1;
+    copy(p1.tags.begin(), p1.tags.end(), inserter(ans.tags, ans.tags.end()));
+    return ans;
+}
+
+int main(){
+    Picture p1, p2;
+    p1.id = 0;
+    p2.id = 1;
+
+    p1.tags = {"pau", "ibrar", "domi"};
+    p2.tags = {"pau", "ibrar", "dani"};
+    Slide ans = create(p1);
+
+
+    for (auto it = ans.tags.begin(); it != ans.tags.end(); ++it) cout << ' ' << *it;
+    cout << endl;
+
+    cout << "Foto 1: " << ans.ph1 << endl;
+    cout << "Foto 2: " << ans.ph2 << endl;
+    
+    
+}
