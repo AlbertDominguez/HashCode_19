fba29fecb2ea472b46ac8ca88a39b88535698911
AlbertDominguez <albertadm99@gmail.com>
2019-02-28 19:42:28 +0100

    Read function added

--- main.cc
+++ main.cc
@@ -1,8 +1,30 @@
+#include "structures.hh"
 #include <iostream>
-
 using namespace std;
 
+vector<Picture> pictures;
+
+void read() {
+    int N; cin >> N;
+    pictures = vector<Picture>(N);
+    for (int i = 0; i < N; ++i) {
+        char v; cin >> v;
+        pictures[i].vertical = (v == 'V');
+        int tagnum; cin >> tagnum;
+        pictures[i].tags = vector<string>(tagnum);
+        for (int j = 0; j < tagnum; ++j) {
+            cin >> pictures[i].tags[j];
+        }
+    }
+}
+
 int main () {
-	
-	
+    read();
+	for (auto pic : pictures) {
+        cout << (pic.vertical ? "V " : "H ") << pic.tags.si
+        
+        ze();
+        for (string s : pic.tags) cout << ' ' << s;
+        cout << endl; 
+    }
 }
